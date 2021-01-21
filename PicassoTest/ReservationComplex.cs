52cb1a79c6877c2e35038a20c2a0a129f242e545
Jose <Jose@DESKTOP-I47R5VA>
2021-01-21 17:33:41 +0100

    Agregar archivos de proyecto.

---
+++ PicassoTest/ReservationComplex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PicassoTest
+{
+    public class ReservationComplex
+    {
+        public User MadeBy { get; set; }
+        public float Price { get; set; }
+        public bool CanBeCancelledBy(User user)
+        {
+            if(user.IsAdmin)
+            {
+                return true;
+            }
+            if(user == MadeBy)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public float PayReservation(UserComplex user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class UserComplex
+    {
+        public float Money { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
